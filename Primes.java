231a6eec7ef39d28571cfa38c615709d7c792ed5
peninahd <peninah.deichman@post.runi.ac.il>
2024-12-01 09:17:09 +0200

    Primes try 1

--- Primes.java
+++ Primes.java
@@ -1,5 +1,43 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int N = Integer.parseInt(args[0]);
+        boolean[] primes = new boolean[N + 1];
+        //sets all elements with index > 1 to true
+        for (int i = 2; i <= N; i++) {
+            primes[i] = true;
+        }
+        //crosses out all multiples of 2
+        int counter = 0;
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            if(primes[i]) {
+                for(int j = i * i; j <= N; j += i) {
+                    primes[j] = false;
+                    counter++;
+                }
+            }
+        }
+
+        int percent = (int) (100 * (N - counter + 2)/ N);
+
+        int index = 0;
+        int[] primeNumbers = new int[N - counter];
+        for (int i = 0; i <= N; i++) {
+            if(primes[i]) {
+                primeNumbers[index] = i;
+                System.out.println(primeNumbers[index]);
+            }
+        }
+
+        System.out.println("There are " + (N - counter + 2) + " primes between 2 and " + N + ". (" +
+            percent + "% are primes)");
+        
+
+
+
+
+
+
+
+
     }
 }

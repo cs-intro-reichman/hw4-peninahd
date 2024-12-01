e0548fbcd6f55e289a3b4be9a037f71e88e46399
peninahd <peninah.deichman@post.runi.ac.il>
2024-12-01 14:25:37 +0200

    fixed coutner again

--- Primes.java
+++ Primes.java
@@ -7,31 +7,36 @@ public class Primes {
             primes[i] = true;
         }
         //crosses out all multiples of 2
-        int counter = 0;
         for (int i = 2; i <= Math.sqrt(N); i++) {
             if(primes[i]) {
                 for(int j = i * i; j <= N; j += i) {
                     primes[j] = false;
-                    counter++;
                 }
             }
         }
+        int primeCounter = 0;
+        for (int i = 2; i <= N; i++) {
+            if (primes[i]) {
+                primeCounter++;
+            }
+        }
         //percentage calculator
-        int percent = (int) (100 * (N - counter - 1)/ N);
+        int percent = (int) (100 * (primeCounter)/ N);
 
         //print leading line
         System.out.println("Prime numbers up to " + N + ":");
 
         int index = 0;
-        int[] primeNumbers = new int[N - counter];
+        int[] primeNumbers = new int[primeCounter];
         for (int i = 0; i <= N; i++) {
             if(primes[i]) {
                 primeNumbers[index] = i;
                 System.out.println(primeNumbers[index]);
+                index++;
             }
         }
 
-        System.out.println("There are " + (N - counter - 1) + " primes between 2 and " + N + " (" +
+        System.out.println("There are " + primeCounter + " primes between 2 and " + N + " (" +
             percent + "% are primes)");
         
     }
